cb69f5e8594ec62cfc99e1dddbecbbe83b9db4a1
Daniel Gallagher <daniel@dangsecurity.com>
2026-02-06 23:37:23 -0500

    Update TFTDisplay.cpp
    Added capability to define different resolution values for the ST7789 memory and physical panel dimensions.

--- src/graphics/TFTDisplay.cpp
+++ src/graphics/TFTDisplay.cpp
@@ -536,6 +536,14 @@ class LGFX : public lgfx::LGFX_Device
             cfg.offset_x = 0;
             cfg.offset_y = 0;                             // No vertical shift needed — panel is top-aligned
             cfg.offset_rotation = 2;                      // Rotate 180° to correct upside-down layout
+#elif defined(ST7789_ALT)
+            cfg.memory_width = TFT_WIDTH;              // Maximum width supported by the driver IC
+            cfg.memory_height = TFT_HEIGHT;            // Maximum height supported by the driver IC
+            cfg.panel_width = TFT_PANEL_WIDTH;         // actual displayable width
+            cfg.panel_height = TFT_PANEL_HEIGHT;       // actual displayable height
+            cfg.offset_x = TFT_OFFSET_X;               // Panel offset amount in X direction
+            cfg.offset_y = TFT_OFFSET_Y;               // Panel offset amount in Y direction
+            cfg.offset_rotation = TFT_OFFSET_ROTATION; // Rotation direction value offset 0~7 (4~7 is mirrored)
 #else
             cfg.memory_width = TFT_WIDTH;              // Maximum width supported by the driver IC
             cfg.memory_height = TFT_HEIGHT;            // Maximum height supported by the driver IC
